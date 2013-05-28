956a1424fb0e1023c0c148e45a30b247e8803b69
karimchebbour <karimchebbour@gmail.com>
2013-05-28 13:12:24 +0200

    Zombie Implementation

--- src/Constants.java
+++ src/Constants.java
@@ -1,5 +1,6 @@
 
 public class Constants 
 {
-
+	public static int ZOMBIE_SPEED_MAX = 4;
+	public static int ZOMBIE_PERCEPTION_MAX = 6;
 }
--- src/Zombie.java
+++ src/Zombie.java
@@ -1,5 +1,56 @@
+import sim.engine.SimState;
+import sim.engine.Steppable;
+import sim.engine.Stoppable;
 
-public class Zombie 
+public class Zombie implements Steppable
 {
+	private int speed = Constants.ZOMBIE_SPEED_MAX;
+	private int perception = Constants.ZOMBIE_PERCEPTION_MAX;
+	private Stoppable stoppable;
+	private boolean isMaximumShot = false;
+	private boolean isAlive = true;
+	private boolean isBlockedForOneStep = false;
+	
+	public enum BODY_PART
+	{
+		ARM,
+		LEG,
+		TRUNK;
+	}
+	
+	@Override
+	public void step(SimState state) {
+		Environment environment = (Environment)state;
+		
+		if(!this.isAlive)
+		{
+		
+			this.stoppable.stop();
+		}
+	}
 
+	public void headShot()
+	{
+		this.isAlive = false;
+	}
+	
+	public void bodyShot(BODY_PART part)
+	{
+		switch(part)
+		{
+			case ARM:
+				if(!this.isMaximumShot) this.speed--;
+				break;
+			case LEG:
+				if(!this.isMaximumShot) this.speed--;
+				else this.isBlockedForOneStep = true;
+				break;
+			case TRUNK:
+				if(!this.isMaximumShot) this.speed--;
+				// ELSE : MOVE BACK 1 CASE
+				break;
+				default:
+					break;
+		}
+	}
 }
